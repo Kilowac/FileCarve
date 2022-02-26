987e2874db58fb06014494a767691f9ccd88b68c
Internet Degenerate <empena1337@gmail.com>
2022-02-26 13:26:18 -0600

    added comments

--- fc.py
+++ fc.py
@@ -1,3 +1,4 @@
+#THIS WILL ONLY CARVE OUT CONTIGUOUS FILES.
 import sys
 import os
 from colorama import init, deinit, Fore, Back, Style
@@ -6,9 +7,16 @@ print(Style.BRIGHT, end = '')
 r = Fore.RED; b = Fore.BLUE; g = Fore.GREEN;
 bl = Fore.BLACK; y = Fore.YELLOW; c = Fore.CYAN;
 m = Fore.MAGENTA; w = Fore.WHITE
+
+#jcon and pcon are the flags that tells the program what type of file(s) to look for
 jcon = False
 pcon = False
+
+#the read path of the image file
 r_path = ''
+
+
+#General information for the user; argument processing
 usage = 'Usage => fc.py [-f <FILEPATH>] [-t <FILETYPE>] [-h]'
 for i in range(len(sys.argv)):
     if '-h' in sys.argv:
@@ -34,18 +42,21 @@ for i in range(len(sys.argv)):
                 exit()
     elif sys.argv[i] == '-t':
         j = i+1
-        for j in range(len(sys.argv)):
-            if sys.argv[j] == 'jpg':
+        for k in range(j,len(sys.argv)):
+            if sys.argv[k] == 'jpg':
                 jcon = True
-            elif sys.argv[j] == 'pdf':
+            elif sys.argv[k] == 'pdf':
                 pcon = True
 
+
+#Verify path's existance
 if r_path == '':
     print(w+usage)
     print('    fc.py: error: the following arguments are required: -f <FILEPATH>')
     print('    Try \'-h\' for help')
     exit()
 
+#default file searching conditions
 if not(jcon) and not(pcon):
     print(bl+'{'+r+'!'+bl+'}'+r+' No type arguments; defaulting to '+w+'.jpg'+r+' and '+w+'.pdf'+r+' files.\n')
     jcon = True
@@ -63,6 +74,7 @@ elif pcon:
 
 print(notify+rquery+g+' files\n')
 
+#Query the user for the writing location; verify and set writing path
 while True:
     print(w+'('+g+'?'+w+')' +w+ ' What path would you like to write the files to?\n'+y+'    >> '+w, end = '')
     w_path = input()
@@ -70,6 +82,7 @@ while True:
         break
     print(bl+'\n['+r+'-'+bl+']'+r+' Path '+w+w_path+r+' does not exist.\n')
 
+#Add '/' if non-existant for file writing; e.g. ./Folder[carved file] -> ./Folder/[carved file]
 if w_path[len(w_path)-1] != '/':
     w_path += '/'
 
@@ -77,6 +90,12 @@ print(w+'\n['+g+'+'+w+']'+g+' Path to '+w+w_path+g+' established.\n')
 
 file = open(r_path, 'rb')
 
+#'build' is the variable that will be building the file to carve
+#'b_proc' means 'build process;' this is a flag to focus on building the file and searching for
+#    the file footer until completion
+#'ext' is the extension of the file, this is used for defining what footer to look for after the file
+#    building has started as well as being used for file writing reasons
+#'pdf' and 'jpg' are for the file writing names
 data = file.read(512)
 build = b''
 b_proc = False
@@ -86,6 +105,7 @@ jpg = 1
 pdf = 1
 file_type = ''
 
+#search for file headers and naming the file to be written while cheking for pre-existing file names
 def hdr(data):
     global ext, jpg, pdf, file_type, w_path
     if jcon:
@@ -109,6 +129,7 @@ def hdr(data):
             return ind
     return -1
 
+#find footer determined by 'ext,' adjust index for offset, return index
 def ftr(data):
     global ext
     if ext == '.jpg':
@@ -121,11 +142,13 @@ def ftr(data):
             ind += 4
     return ind
 
+#Starting index of the file header; 'f' is for file writing
 strt = -1
 f = None
 
 print(g+'('+y+'?'+g+')'+g+' Searching for '+w+rquery+g+' headers...\n')
 
+#Search for headers, if return is not -1, set b_proc flag, build file, and look for the footer
 while data:
     if not(b_proc):
         if hdr(data) != -1:
@@ -137,6 +160,10 @@ while data:
             f = open(w_file, "wb")
     if b_proc:
         end = ftr(data)
+        #if the footer is not within the 512 byte sector contatonate the sector from strt to end, set strt to 0
+        #    as this program is currently assuming the file is contifuous. Else, if footer is found, write from strt
+        #    to the footer (including the footer as a result of the index offset)
+        #Write to file
         if end == -1:
             s1 = slice(strt,len(data))
             build += data[s1]
@@ -152,8 +179,8 @@ while data:
             footer = b''
             f.close()
             print(g+'('+y+'?'+g+')'+g+' Searching for '+w+rquery+g+' headers...\n')
-
     data = file.read(512)
+
 print(w+'['+g+'+'+w+']'+' Program Completed.\n')
 file.close()
 deinit()
